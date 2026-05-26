dba3c66b62d668b0d3e15e8ead1838965b540fa2
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2026-05-26 10:55:05 +0700

    feat: add HTTP server and GET movies endpoint use Mux Route

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+	mux.Handle("GET /", http.FileServer(http.Dir("static")))
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+var movies = []movieResponse{
+	{ID: "inception", Title: "Inception", Rows: 5, SeatsPerRow: 8},
+	{ID: "dune", Title: "Dune: Part Two", Rows: 4, SeatsPerRow: 6},
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) {
+	WriteJSON(w, http.StatusOK, movies)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(v)
+}
+
+type movieResponse struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Rows        int    `json:"rows"`
+	SeatsPerRow int    `json:"seats_per_row"`
 }
